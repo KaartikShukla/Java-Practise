14ccf9a0025c74a8c7a49781d22d89bcd1f5caa3
KaartikShukla <kaartikshukla77@gmail.com>
2026-09-07 16:51:07 +0530

    commit -II

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -1,4 +1,16 @@
 import java.util.*;
+class Student{
+    int roll_no;
+    String name;
+    Student(int roll_no,String name){
+        this.roll_no = roll_no;
+        this.name = name;
+    }
+    
+    public String toString(){
+        return "["+ this.name + " " + this.roll_no + "]";
+    }
+}
 public class ArrayListDemo {
     public static void main(String[] args){
         ArrayList<Integer> list = new ArrayList<>();
@@ -9,5 +21,23 @@ public class ArrayListDemo {
         System.out.println(list.get(1));
         list.add(1,5);
         System.out.println(list);
+        list.set(1,6);
+        System.out.println(list);
+        //list.addFirst(10);
+        //list.addLast(20);
+        List<Integer> lis = new ArrayList<>();
+        lis.add(2);
+        lis.add(3);
+        System.out.println(lis.get(1));
+        lis.add(1,4);
+        System.out.println(lis);
+        lis.set(1,5);
+        System.out.println(lis);
+        //list.addFirst(10);
+        //list.addLast(20);
+        List<Student> list2 = new ArrayList<>();
+        list2.add(new Student(1,"Raj"));
+        list2.add(new Student(2,"Rajesh"));
+        System.out.println(list2);
     }
 }
